7092eb5e367ffd03a8579e7df55481b3da20922d
YmendezM <ysmendezm@gmail.com>
2020-03-23 16:48:45 -0300

    navigate login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB-e_jMcYQD-GNo9ltKi9p41FQC0dW6tnU',
+    authDomain: 'auth-65c46.firebaseapp.com',
+    databaseURL: 'https://auth-65c46.firebaseio.com',
+    projectId: 'auth-65c46',
+    storageBucket: 'auth-65c46.appspot.com',
+    messagingSenderId: '71655619080'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB-e_jMcYQD-GNo9ltKi9p41FQC0dW6tnU',
+    authDomain: 'auth-65c46.firebaseapp.com',
+    databaseURL: 'https://auth-65c46.firebaseio.com',
+    projectId: 'auth-65c46',
+    storageBucket: 'auth-65c46.appspot.com',
+    messagingSenderId: '71655619080'
+  }
+ 
 };
 
 /*
